e92427a0ce9e193004f03e5131b77acbc5221d89
Daniel-Espinoza-unemi <112334573+Daniel-Espinoza-unemi@users.noreply.github.com>
2022-08-28 14:07:55 -0500

    Add files via upload

---
+++ Estructura.js
@@ -0,0 +1,11 @@
+var nombre="Daniel, "
+let apellido="Espinoza, "
+const edad="19 años, "
+const curso = " Curso A2, "
+var materia= "Estructura de datos. "
+console.log(nombre,apellido,edad,curso,materia)
+
+console.log("Hola")
+console.log("Ingeniera")
+console.log("Espero y le guste")
+console.log("La pagina")
